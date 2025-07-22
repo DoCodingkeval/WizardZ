4d3ca0aa022bd50dd688e7674b610e0ba3ed61ef
DoCodingkeval <gohelkeval7@gmail.com>
2025-07-22 17:38:27 +0530

    commit

--- script.js
+++ script.js
@@ -1,89 +1,125 @@
+const openMenu = document.querySelector(".header__menu");
+const closeMenu = document.querySelector(".close__menu");
+const navbar = document.querySelector(".navbar");
+
+openMenu.addEventListener("click", () => {
+  navbar.classList.add("active");
+});
+
+closeMenu.addEventListener("click", () => {
+  navbar.classList.remove("active");
+});
+
 // ----------GSAP ANIMATION---------------------
-function PageOneAnime(){
-    var timeline = gsap.timeline()
+function PageOneAnime() {
+  var timeline = gsap.timeline();
 
-timeline.from(".header__logo",{
+  timeline.from(".header__logo", {
     y: -100,
-    opacity:0,
-    duration:0.8,
-    ease: "bounce.out"
-})
+    opacity: 0,
+    duration: 0.8,
+    ease: "bounce.out",
+  });
 
-timeline.from(".nav__items li",{
+  timeline.from(".nav__items li", {
     y: 50,
     rotation: 15,
     opacity: 0,
     duration: 0.6,
     stagger: 0.1,
-    ease: "back.out(1.7)"
-})
-timeline.from(".hero__leftsection",{
-    x:-100,
-    opacity: 0,
-    duration: 0.7,
-    ease: "power2.out"
-},"same")
-timeline.from(".hero__rightsection",{
-    scale: 0.7,
-    rotation: -5,
-    opacity: 0,
-    duration: 0.7,
-    ease: "power2.out"
-},"same")
-timeline.from(".brand__images",{
+    ease: "back.out(1.7)",
+  });
+  timeline.from(
+    ".hero__leftsection",
+    {
+      x: -100,
+      opacity: 0,
+      duration: 0.7,
+      ease: "power2.out",
+    },
+    "same"
+  );
+  timeline.from(
+    ".hero__rightsection",
+    {
+      scale: 0.7,
+      rotation: -5,
+      opacity: 0,
+      duration: 0.7,
+      ease: "power2.out",
+    },
+    "same"
+  );
+  timeline.from(".brand__images", {
     y: -50,
-    opacity:0,
+    opacity: 0,
     duration: 0.5,
     stagger: 0.1,
-    ease: "power1.out"
-})
-gsap.to(".brand__images",{
+    ease: "power1.out",
+  });
+  gsap.to(".brand__images", {
     y: "-=10",
     duration: 1.5,
     yoyo: true,
     repeat: -1,
-    ease: "sine.inOut"
-})
+    ease: "sine.inOut",
+  });
 }
-function PageSecondAnime(){
-    var timeline = gsap.timeline({
-        scrollTrigger:{
-            trigger: ".services__section",
-            scroller: "body",
-            start: "top 20%",
-            toggleActions: "play none none reverse"
-        }
-    })
-    timeline.from(".each__card:nth-child(1)",{
-        x: -100,
-        rotateY: 90,
-        opacity: 0,
-        duration: 0.8,
-        ease: "power2.out"
-    },"same")
-    timeline.from(".each__card:nth-child(2)",{
-        x: 100,
-        rotateY: -90,
-        opacity: 0,
-        duration: 0.8,
-        ease: "power2.out"
-    },"same")
-    timeline.from(".each__card:nth-child(3)",{
-        x: -100,
-        rotateY: 90,
-        opacity: 0,
-        duration: 0.8,
-        ease: "power2.out"
-    },"same")
-    timeline.from(".each__card:nth-child(4)",{
-        x: 100,
-        rotateY: -90,
-        opacity: 0,
-        duration: 0.8,
-        ease: "power2.out"
-    },"same")
+function PageSecondAnime() {
+  var timeline = gsap.timeline({
+    scrollTrigger: {
+      trigger: ".services__section",
+      scroller: "body",
+      start: "top 20%",
+      toggleActions: "play none none reverse",
+    },
+  });
+  timeline.from(
+    ".each__card:nth-child(1)",
+    {
+      x: -100,
+      rotateY: 90,
+      opacity: 0,
+      duration: 0.8,
+      ease: "power2.out",
+    },
+    "same"
+  );
+  timeline.from(
+    ".each__card:nth-child(2)",
+    {
+      x: 100,
+      rotateY: -90,
+      opacity: 0,
+      duration: 0.8,
+      ease: "power2.out",
+    },
+    "same"
+  );
+  timeline.from(
+    ".each__card:nth-child(3)",
+    {
+      x: -100,
+      rotateY: 90,
+      opacity: 0,
+      duration: 0.8,
+      ease: "power2.out",
+    },
+    "same"
+  );
+  timeline.from(
+    ".each__card:nth-child(4)",
+    {
+      x: 100,
+      rotateY: -90,
+      opacity: 0,
+      duration: 0.8,
+      ease: "power2.out",
+    },
+    "same"
+  );
 }
-if(window.innerWidth > 768){
-PageOneAnime()
-PageSecondAnime()
+if (window.innerWidth > 768) {
+  PageOneAnime();
+  PageSecondAnime();
 }
